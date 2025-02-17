520e05938edb7dc57c081e0f7515cae3d10c26ad
unknown <huychuoi05@gmail.com>
2025-02-17 11:02:31 +0700

    First commit

---
+++ day1/let_demo.js
@@ -0,0 +1,15 @@
+// let 
+let x=100;
+if(x===100){
+
+
+}
+// x ngoai
+console.assert.log( "x ben ngoai if:",x);
+//=================
+/*
+x=100
+y="100";
+z=(x==y); => z=true
+z+ (x==y) ==> z=false;
+\*
