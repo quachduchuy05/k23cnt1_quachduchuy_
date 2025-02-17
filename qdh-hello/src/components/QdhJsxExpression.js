6a1f9f6173ab317b3106bcaea70fad9a9f5e555d
quachduchuy05 <huychuoi05@gmail.com>
2025-02-17 16:49:36 +0700

    add file

---
+++ qdh-hello/src/components/QdhJsxExpression.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+export default function QdhJsxExpression(){
+    // bien
+    const name="quach duc huy";
+// bien doi tuong
+    const user={
+        firstName:"quach"
+        lastName:"huy";
+
+    }
+    // ham
+    const fullName=(user)=>{
+            return "<h2>" + user.firstName +' '+ user.lastName + "</h2>";
+
+    }
+    //element
+    const element = (
+        <div>
+            {/*bieu thuc jsx */}
+            {fullName(user)}
+            <hr/>
+            <h3> Wecome to, {name} </h3>
+        </div>
+    )
+    //ham
+    const sayWelcome =(name)=>{
+        if(name){
+            return <h3>Welcome to {name}</h3>
+        }else{
+            return <h3>Welcome to  fit-ntu-k23cnt1</h3>
+        }
+    }
+    return (
+        <div>
+                <h1> Qdh - Jsx Expression</h1>
+                {/* su dung bien element*/}
+                {element}
+                <hr/>
+                {sayWelcome()}
+                <hr/>
+                {sayWelcome("quach duc huy")}
+
+        </div>
+    )
+}
