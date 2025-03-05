5f01859170327963380852ba33953c6a255985a6
quachduchuy05 <huychuoi05@gmail.com>
2025-03-05 22:40:35 +0700

    Initial commit

---
+++ qdh-hello/src/components/QdhClassComp.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class QdhClassComp extends component {
+    render(){
+        return (
+            <div>
+                <h1>day la class component</h1>
+                <p>
+                    k23cnt1 - reactJs class components
+                </p>
+            </div>
+        );
+    }
+}
---
+++ qdh-hello/src/components/QdhFuncComp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function QdhFuncComp() {
+    return(
+        <div>
+            <h1> day laf function component</h1>
+            <p> quach duc huy</p>
+        </div>
+    )
+}
+
